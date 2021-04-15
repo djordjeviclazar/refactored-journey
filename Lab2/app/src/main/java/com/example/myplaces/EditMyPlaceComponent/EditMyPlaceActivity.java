7ea6f7d0c6ba1e6fe3bff4bfa40be566e9f2e5ce
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2021-04-16 01:10:21 +0200

    Added click listener, EditText, removed floating button

--- Lab2/app/src/main/java/com/example/myplaces/EditMyPlaceComponent/EditMyPlaceActivity.java
+++ Lab2/app/src/main/java/com/example/myplaces/EditMyPlaceComponent/EditMyPlaceActivity.java
@@ -1,7 +1,10 @@
 package com.example.myplaces.EditMyPlaceComponent;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.myplaces.Models.MyPlace;
+import com.example.myplaces.Models.MyPlacesData;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,10 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.myplaces.R;
 
-public class EditMyPlaceActivity extends AppCompatActivity {
+public class EditMyPlaceActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,37 @@ public class EditMyPlaceActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button finishBtn = (Button) findViewById(R.id.editmyplace_finish_btn);
+        finishBtn.setOnClickListener(this);
+        Button cancelBtn = (Button) findViewById(R.id.editmyplace_cancel_btn);
+        cancelBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.editmyplace_finish_btn:
+            {
+                EditText editName = (EditText) findViewById(R.id.editmyplace_name_edit);
+                String newName = editName.getText().toString();
+                EditText editDesc = (EditText) findViewById(R.id.editmyplace_desc_edit);
+                String newDesc = editDesc.getText().toString();
+
+                MyPlace place = new MyPlace(newName, newDesc);
+                MyPlacesData.getInstance().addNewPlace(place);
+
+                setResult(Activity.RESULT_OK);
+                finish();
+                break;
             }
-        });
+
+            case R.id.editmyplace_cancel_btn:
+            {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+                break;
+            }
+        }
     }
 }
--- Lab2/app/src/main/java/com/example/myplaces/EditMyPlaceComponent/First3Fragment.java
+++ Lab2/app/src/main/java/com/example/myplaces/EditMyPlaceComponent/First3Fragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.myplaces.R;
+
 public class First3Fragment extends Fragment {
 
     @Override
